3b98a0be4890e1262872c5770a313edc94558f43
BorowskiRadek <45455509+radzik100@users.noreply.github.com>
2020-05-24 15:41:16 +0200

    While + klasa do obliczania spalania

---
+++ src/main/java/Main.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        RoadData roadData = new RoadData(1,0,0);
+
+
+        int totalFuel = 0;
+        int totalDistance = 0;
+
+
+        while (roadData.getNumber() > 0) {
+
+            System.out.println("Aby zakończyć wpisz 0");
+            System.out.print("Wpisz numer trasy: ");
+
+
+            int theNumber = input.nextInt();
+            roadData.setNumber(theNumber);
+
+            if (theNumber != 0) {
+
+
+                System.out.print("Wpisz liczbę przejechanych kilometrów: ");
+                int theDistance = input.nextInt();
+                roadData.setDistance(theDistance);
+
+                System.out.print("Wpisz spaleone paliwo: ");
+                int theFuel = input.nextInt();
+                roadData.setFuel(theFuel);
+
+                double average = (double) roadData.getFuel() * 100 / roadData.getDistance();
+                totalDistance = totalDistance + theDistance;
+                totalFuel = totalFuel + theFuel;
+
+
+                System.out.printf("Srednie spalanie z tej trasy wynosi: %.2f l/100km %n",average);
+
+            }
+
+            else {
+                double totalAverage = (double) totalFuel * 100 / (double) totalDistance;
+                System.out.printf("Srednia ze wszystkich przejazdów: %.2f l/100 km %n",totalAverage);
+
+            }
+
+
+
+        }
+
+    }
+}
